46fd712ac58b5ce0ebe21ad47799e4818b565724
Asin <asinkanil04@gmail.com>
2026-06-10 20:01:12 +0530

    student informations

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+student={} 
+student["name"]=input("Enter name: ")
+student["age"]=input("Enter age: ")
+student["course"]=input("Enter course: ")
+print("\nstudent informations:")
+print("Name: ", student["name"])
+print("Age: ", student["age"])
+print("Course: ", student["course"])
